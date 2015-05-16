b4c74393ca57c6abf2525b64ed6a0cb586678338
rbtchc <rbtchc@gmail.com>
2015-05-17 00:12:45 +0800

    "./install.sh true" to install fake mraa.py to /opt/diaperpie/ when h/w not available

--- edison/install.sh
+++ edison/install.sh
@@ -13,6 +13,10 @@ test-alert:$_target_folder:0755 \
 diaperpie.service:/lib/systemd/system:0644 \
 "
 
+if [ "x$1" == "xtrue" ]; then
+_file="${_file}\nmraa.py:$_target_folder:0644"
+fi
+
 for _f in $_file; do
   _s=`echo $_f | cut -d ':' -f 1`
   _d="`echo $_f | cut -d ':' -f 2`/$_s"
